e65748bbbda5ecfcebca0c26de19066bf1c87f52
Emma <emmagsalisbury@gmail.com>
2023-03-23 15:09:48 +0000

    Updated javaScript

--- src/script.js
+++ src/script.js
@@ -1,6 +1,23 @@
+
+
+  function darkMode() {
+    let body = document.querySelector("body");
+    body.classList.remove("light");
+    let button = document.querySelector("button");
+    button.innerHTML = "Light Mode";
+    button.addEventListener("click", showLight);
+
+  }
+ 
+  
   function showLight() {
         let body = document.querySelector("body");
         body.classList.add("light");
+        let button = document.querySelector("button");
+        button.innerHTML = "Dark Mode";
+        button.addEventListener("click", darkMode);
+        
+      
     }
     let button = document.querySelector("button");
     button.addEventListener("click", showLight);
